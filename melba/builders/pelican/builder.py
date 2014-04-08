5392876bf1eed6a4188a98de5c8666603a06b1aa
averagehuman <cyclebelfast@gmail.com>
2014-04-08 11:19:48 +0100

    working Pelican build and test

--- melba/builders/pelican/builder.py
+++ melba/builders/pelican/builder.py
@@ -17,15 +17,17 @@ PLUGIN_PATH = pathjoin(dirname(__file__), 'plugins')
 MELBA_THEMES = pathjoin(dirname(__file__), 'themes')
 
 def build(src, dst, options, local_settings, stdout=sys.stdout, stderr=sys.stderr):
+    """The main Pelican build function"""
     theme_path = options.get('theme_path', MELBA_THEMES)
     settings = get_settings(src, dst, theme_path, local_settings)
     pelican = Pelican(settings)
     pelican.run()
 
 def get_settings(src, dst, theme_path, overrides):
+    """Generate Pelican settings dict and normalize certain options"""
     settings = copy.deepcopy(DEFAULT_CONFIG)
     settings.update(overrides)
-    settings['PATH'] = src
+    settings['PATH'] = norm_content_path(src, settings['PATH'])
     settings['OUTPUT_PATH'] = dst
     settings['PLUGIN_PATH'] = PLUGIN_PATH
     settings['DELETE_OUTPUT_DIRECTORY'] = False
@@ -40,7 +42,29 @@ def get_settings(src, dst, theme_path, overrides):
     global PYGMENTS_RST_OPTIONS
     PYGMENTS_RST_OPTIONS = settings.get('PYGMENTS_RST_OPTIONS', None)
     settings['PELICAN_CLASS'] = 'pelican.Pelican'
-    return settings
+    #updated = {}
+    #for key, val in settings.items():
+    #    if key in STATIC_PATH_SETTINGS:
+    #        updated[key] = norm_path_setting(src, val)
+    #settings.update(updated)
+    return configure_settings(settings)
+
+def norm_content_path(root, path):
+    """Ensure the PATH setting is a subdirectory of the repository root"""
+    if not path:
+        src = None
+    if isabs(path):
+        if path.startswith(root):
+            src = path
+        else:
+            src = None
+    else:
+        src = pathjoin(root, path)
+    if src and pathexists(src):
+        return src
+    else:
+        # PATH was not set or badly set, fallback to returning the repository root
+        return root
 
 
 
--- melba/tests/test_pelican.py
+++ melba/tests/test_pelican.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 
 import os
 import tempfile
@@ -14,6 +17,25 @@ def test_pelican_build(storage_root):
     src = pathjoin(storage_root, 'pelican', 'samples', 'advanced')
     dst = tempfile.mkdtemp(prefix="melba-")
     assert pathexists(src)
-    build_pelican(src, dst, {}, {})
-    assert dst == None
+    settings = {}
+    settings["AUTHOR"] = 'Alexis Métaireau'
+    settings["SITENAME"] = "Alexis' log"
+    settings["SITEURL"] = 'http://blog.notmyidea.org'
+    settings["TIMEZONE"] = "Europe/Paris"
+    settings["RELATIVE_URLS"] = True
+    settings["PATH"] = 'content'
+    settings["STATIC_PATHS"] = [
+        'pictures',
+        'cat1',
+        'extra/robots.txt',
+    ]
+    settings["PYGMENTS_RST_OPTIONS"] = {'linenos': 'table'}
+    settings["REVERSE_CATEGORY_ORDER"] = True
+    settings["LOCALE"] = "C"
+    settings["DEFAULT_PAGINATION"] = 4
+    settings["DEFAULT_DATE"] = (2012, 3, 2, 14, 1, 1)
+    assert not os.listdir(dst)
+    build_pelican(src, dst, {}, settings)
+    assert os.listdir(dst)
+    shutil.rmtree(dst)
 
